99039f2db3221d7571ac4631f0d09df2efdaa635
mihail.razin <razin097@gmail.com>
2019-11-18 19:34:50 +0300

    code style

--- SberbankTestTask/viewModel/TableViewModel.swift
+++ SberbankTestTask/viewModel/TableViewModel.swift
@@ -10,45 +10,48 @@ import Foundation
 
 
 class TableViewModel: TableViewModelType {
-    
+
     //MARK: - var, let, property
-    private var delegate:UpdateTableDelegate?
+    private var delegate: UpdateTableDelegate?
     private var publications: [Publication]?
     private var maxNumberOfRows = numberOfRowsInEachPart
-    
+
     //MARK: - init
-    required init(delegate:UpdateTableDelegate) {
+    required init(delegate: UpdateTableDelegate) {
         self.delegate = delegate
     }
-    
+
     //MARK: - rows counting
     var numberOfRows: Int {
-        guard let publications = publications else {return 0}
+        guard let publications = publications else { return 0 }
         if publications.count > self.maxNumberOfRows {
             return maxNumberOfRows
         }
         return publications.count
     }
-    
-    func resetMaxNumberOfRows(){
+
+    func resetMaxNumberOfRows() {
         self.maxNumberOfRows = numberOfRowsInEachPart
     }
-    
+
     //MARK: - json get & parse
     public func refreshData(searchWord: String) {
-        //q=bitcoin&from=2019-10-13&sortBy=publishedAt&apiKey=
+        getJsonFromApi(searchWord: searchWord)
+    }
+
+    private func getJsonFromApi(searchWord: String) {
         let urlString = urlPrefix + "q=\(searchWord == "" ? "news" : searchWord)&from=\(fromDate)&sortBy=publishedAt&apiKey=\(apiKey)"
         print(urlString)
-        guard let url = URL(string: urlString) else {return}
+        guard let url = URL(string: urlString) else { return }
         let task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
 
-                guard let data = data else { return }
-                do {
-                    let decoder = JSONDecoder()
-                    let response = try decoder.decode(PublicationsStruct.self, from: data)
-                    self.parsePublications(publs: response)
-                } catch { print(error) }
-            })
+            guard let data = data else { return }
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(PublicationsStruct.self, from: data)
+                self.parsePublications(publs: response)
+            } catch { print(error) }
+        })
         task.resume()
     }
 
@@ -68,14 +71,14 @@ class TableViewModel: TableViewModelType {
         self.publications = arr
         self.updateTable()
     }
-    
+
     //MARK: - load more
-    func loadMore(searchWord: String?){
+    func loadMore(searchWord: String?) {
         if self.moreRows() == true {
             self.refreshData(searchWord: searchWord ?? defaultSearchWord)
         }
     }
-    
+
     func moreRows() -> Bool {
         let buf = self.maxNumberOfRows
         self.maxNumberOfRows += numberOfRowsInEachPart
@@ -84,18 +87,18 @@ class TableViewModel: TableViewModelType {
         }
         return buf != self.maxNumberOfRows
     }
-    
+
     //MARK: - view models
     func cellViewModel(forRow: Int) -> TableCellViewModelType {
-        let publication:Publication? = publications?[forRow]
+        let publication: Publication? = publications?[forRow]
         return TableCellViewModel(publication: publication)
     }
-    
+
     func detailViewModel(forRow: Int) -> DetailViewModelType {
-        let publication:Publication? = publications?[forRow]
+        let publication: Publication? = publications?[forRow]
         return DetailViewModel(publication: publication)
     }
-    
+
     //MARK: - delegate
     private func updateTable() {
         DispatchQueue.main.async {
