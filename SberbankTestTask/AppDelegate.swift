cbef6aa8453edfea04534f3085ef9c37e0c38915
mihail.razin <razin097@gmail.com>
2019-11-26 13:28:08 +0300

    clean user defaults if needed

--- SberbankTestTask/AppDelegate.swift
+++ SberbankTestTask/AppDelegate.swift
@@ -16,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
+        
+        if UserDefaults.standard.object(forKey: "watched") != nil {
+            UserDefaults.standard.removeObject(forKey: "watched")
+        }
+        
         return true
     }
 
